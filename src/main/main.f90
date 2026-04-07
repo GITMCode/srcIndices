4f3454ed46a7be4e8c242dacea2762a37ac8c449
Aaron Bukowski <abukowski21@gmail.com>
2026-04-07 15:59:36 -0400

    Fix typos, layout, digest-ibility of main

--- src/main/main.f90
+++ src/main/main.f90
@@ -7,22 +7,25 @@ program testIO
 
   type(TimeType) :: now
   real(Real8_) :: JD
-  print*, "hello, world!"
+  print*, "Beginning main..."
 
-  print*, "setting time to 2011-03-15 00:01:00..."
+  print*, "> setting time to 2011-03-16 00:12:15.0"
   now%iYear = 2011
   now%iMonth = 3
   now%iDay = 16
   now%iHour = 0
   now%iMinute = 12
   now%iSecond = 15
-
   print*, now
+
+  print*, "> converting to real"
   call time_int_to_real(now)
   print*, now
-
-  print*, 'Testing (incorrect) usage of day of year conversion'
-  print*, 'This should not work...'
+  print*, "> current 'time' is: ", now%String
+  print*, "-------------"
+  print*, ""
+  print*, '> Testing (incorrect) usage of day of year conversion'
+  print*, '> This should not work...'
   now%iMonth = 2
   now%iDay = 160
   call time_int_to_real(now)
@@ -34,17 +37,20 @@ program testIO
     print*, "Everything is ok. It shouldn't be!"
   else
     print*, "Correctly found the time error. Nice!!"
-    ! call flush_errors
+    call flush_errors
   endif
 
-  print*, "Testing day of year:"
+  print*, "-------------"
+  print*, ""
+  print*, "> Testing day of year:"
 
   now%iMonth = 1
   now%iDay = 160
   call time_int_to_real(now)
   print*, now
 
-  call report_errors
 
+
+  call report_errors
 
 end program
