0e45a77e6001d5391c7022084a42be3f6f6c76bb
Aaron Bukowski <abukowski21@gmail.com>
2026-04-09 14:35:32 -0400

    Overhaul main:
    - Add helpful comments about what we're doing
    - Remove unnecessary printing
    - Consolidate example usage to be read -> check -> retrieve
    - Final use-case of retrieval is a table that's printed out

--- src/main/main.f90
+++ src/main/main.f90
@@ -10,10 +10,14 @@ program testIO
   integer :: iIndex
   character(50) :: indexName
   real :: f107val, f107aval
-  real :: ae, au, al, bx, by, bz
+  real :: ae, au, al, bx, by, bz, vx, den, t, hpi, hpin
   integer :: iMM, iDD, iAL, iBz
 
   print *, "Beginning main..."
+
+  ! ================================================ !
+  ! Time testing
+  ! ================================================ !
 
   print *, "> setting time to 2011-03-16 00:12:15.0"
   now%iYear = 2011
@@ -63,13 +67,23 @@ program testIO
 
   call report_errors
 
+  ! ================================================ !
+  ! Reading & retrieving actual indices
+  ! ================================================ !
+  ! This is testing and demonstrating the available methods
+  ! Below this block is an example of how the library could actually be used
+
+  ! F107 first...
   print *, "------------------------"
   print *, ""
   print *, "> Reading f107 file..."
   call init_f107("data/f107.txt")
-  call report_errors
-
-  if (.not. isOk) stop
+  ! Good idea to make sure there are no errors after init
+  ! isOk = F if anything has errored
+  if (.not. isOk) then
+    call report_errors
+    stop
+  endif
 
   print *, "> f107 file read successfully!"
   print *, "> Number of f107 values: ", allIndices(1)%nValues
@@ -89,7 +103,7 @@ program testIO
   call get_index(1, now%Time, f107val)
   print *, ">> f107 at 2011-03-16 12:00 = ", f107val
 
-  ! Test set_time + get_index without time
+  ! set_time, then get_index without specifying time
   print *, ""
   print *, "> Testing set_time + get_index..."
   call set_time(now%Time)
@@ -97,7 +111,7 @@ program testIO
 
   print *, ">> f107 (via set_time)    = ", f107val
 
-  ! Test set_time + get_index without specifying time in call
+  ! Can retrieve indices with str or integers
   print *, ""
   print *, "> Testing set_time + get_index with chars..."
   call set_time(now%Time)
@@ -105,11 +119,13 @@ program testIO
 
   print *, ">> f107 (via chars & set_time)    = ", f107val
 
+  ! For completeness, can also specify str & time in call to get_index
   print *, ""
   print *, "> Testing get_index with manual time & chars..."
   call get_index("f107", now%time, f107val)
   print *, ">> f107 (via chars & real-time)    = ", f107val
 
+  ! This is to verify that things are working. Easy to compare with 'tail f107.txt'
   print *, ""
   print *, "> Getting the last few times in the F107 file. Verify these!"
   ! reset time
@@ -130,18 +146,55 @@ program testIO
     enddo
   enddo
 
+  ! Example of incorrect usage. AE has not been read yet!
   print *, ""
   print *, " > here's a check for data we should not have"
   call get_index("ae", ae)
   print *, "AE= ", ae, " ... isOK:", isOk
-  call flush_errors
+  call flush_errors ! Use this sparingly. Will clear stored errors!
 
   print *, ""
-  print *, " >OK cool now read the SME data & look at it"
+  print *, ""
+
+
+  ! ================================================ !
+  ! Example of actual usage
+  ! ================================================ !
+  
+  ! Typical use case is to read all the indices, then start the model
+  ! Time is set on the module-level so can be used for multiple calls to get_index
+  ! isOk will be T if there are no errors, and any error will toggle it to F and persist
+  !   so multiple indices can be read without needing to check 'isOk' ifter each.
+
+  ! Read AE and IMF data
+  print *, "> Reading AE and IMF data..."
   call init_ae("data/ae20021221.dat")
-  ! Check how many pts were read in
+  call init_imf("data/imf20021221.dat")
+  ! Then we'll set HPI to be derived from AE
+  iAE = decode_index('ae')
+  call init_hpi(allIndices(iAE))
+  ! then check if all is ok
+  if (isOk) then
+    print *, "> Read was successful!"
+  else
+    print *, " -->> Read was unseccessful."
+    call report_errors
+    stop
+  endif
+
+  ! Check how many pts were read in. Could check any index, really...
   iAL = decode_index('al')
-  print *, "nPts AL = ", allIndices(iAL)%nValues, " ... isOK:", isOk
+  print *, "> nPts AL = ", allIndices(iAL)%nValues, " ... isOK:", isOk
+  print*, ''
+  print*, ''
+
+  print *, " > Table of a few things read in:"
+  
+  ! header
+  write(*, "(A19, 11(A10))") 'time', 'IMF Bx', 'IMF By', 'IMF Bz', &
+                             'SW Vx', 'SW den', 'SW Temp', &
+                             'AE', 'AU', 'AL', 'HPI', 'HPI_NH'
+  ! Set (most of the) date
   now%iYear = 2002
   now%iMonth = 12
   now%iDay = 21
@@ -149,32 +202,23 @@ program testIO
   now%iSecond = 0
   now%fracSecond = 0.0d0
   do iMM = 50, 59
+    ! Set minute....
     now%iMinute = iMM
+    ! Tell library what time it is
     call set_time(now)
-    call get_index('ae', ae)
-    call get_index('al', al)
-    call get_index('au', au)
-    print *, "min:", now%iMinute, 'ae', ae, 'al', al, 'au', au
-  enddo
-
-  print *, " > Same thing with the omni data. Last few lines (compare with tail)"
-  call init_imf("data/imf20021221.dat")
-  ! Check how many pts were read in
-  iBz = decode_index('al')
-  print *, "nPts Bz = ", allIndices(iBz)%nValues, " ... isOK:", isOk
-  now%iYear = 2002
-  now%iMonth = 12
-  now%iDay = 22
-  now%iHour = 23
-  now%iSecond = 0
-  now%fracSecond = 0.0d0
-  do iMM = 50, 59
-    now%iMinute = iMM
-    call set_time(now)
+    ! Get the indices we want to print
     call get_index('imfbx', bx)
     call get_index('imfby', by)
     call get_index('imfbz', bz)
-    print *, "time:", now%String, '  bx', bx, 'by', by, 'bz', bz
+    call get_index('swvx', vx)
+    call get_index('swn', den)
+    call get_index('swt', t)
+    call get_index('ae', ae)
+    call get_index('au', au)
+    call get_index('al', al)
+    call get_index('hpi', hpi)
+    call get_index('hpin', hpin)
+    write(*, "(A19, 11(F10.1))") now%String,  bx, by, bz, vx, den, t, ae, au, al, hpi, hpin
   enddo
   
 
